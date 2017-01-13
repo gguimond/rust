aaef2da7c17df240e2551e0dae4fb3450601f433
Guillaume Guimond <guimog@gmail.com>
2017-01-13 15:49:10 +0100

    primitive types and comments

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,5 @@
+//! # Rust practice
+
 fn main() {
 	//variable
 	let (a,b) = (1,2);
@@ -16,9 +18,35 @@ fn main() {
     let f: fn(i32, i32) -> i32 = add;
 
     println!("{}", f(1,2));
+
+    let x = true;
+    let y: bool = false;
+
+    let x = 'x';
+
+    let slice = [0, 1, 2, 3, 4];
+    let middle = &slice[1..4];
+
+    println!("{}", middle[0]);
+
+    let x: (i32, &str) = (1, "hello");
+
+    let mut x = (1, 2); 
+	let y = (2, 3);
+	x = y;
+
+	let (_x,_y) = x;
+	println!("{}", _x);
+	println!("{}", x.0);
+
+	assert_eq!(6, add(5,1));
 }
 
 //function
+/// assert_eq!(6, add(5,1));
+/// # fn add(x :i32, y :i32) -> i32{
+/// #     x + y
+/// # }
 fn add(x :i32, y :i32) -> i32{
 	x + y
 }
