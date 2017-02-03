efd2dfeff3b3034fb3628dd086649a56529015c1
Guillaume Guimond <guimog@gmail.com>
2017-02-03 15:01:19 +0100

    method

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -277,6 +277,80 @@ fn main() {
 	    ref mut mr => println!("Got a mutable reference to {}", mr),
 	}
 
+	struct Circle {
+	    x: f64,
+	    y: f64,
+	    radius: f64,
+	}
+
+	impl Circle {
+	    fn area(&self) -> f64 {
+	        std::f64::consts::PI * (self.radius * self.radius)
+	    }
+	    fn reference(&self) ->  &Circle{
+	       println!("taking self by reference!");
+	       self
+	    }
+
+	    fn mutable_reference(&mut self) {
+	       println!("taking self by mutable reference!");
+	    }
+
+	    fn takes_ownership(self) {
+	       println!("taking ownership of self!");
+	    }
+
+	    fn new(x: f64, y: f64, radius: f64) -> Circle {
+	        Circle {
+	            x: x,
+	            y: y,
+	            radius: radius,
+	        }
+	    }
+	}
+
+	struct CircleBuilder {
+	    x: f64,
+	    y: f64,
+	    radius: f64,
+	}
+
+	let mut c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
+	c = Circle::new(0.0, 0.0, 2.0);
+    println!("{}", c.reference().area());
+
+	impl CircleBuilder {
+	    fn new() -> CircleBuilder {
+	        CircleBuilder { x: 0.0, y: 0.0, radius: 1.0, }
+	    }
+
+	    fn x(&mut self, coordinate: f64) -> &mut CircleBuilder {
+	        self.x = coordinate;
+	        self
+	    }
+
+	    fn y(&mut self, coordinate: f64) -> &mut CircleBuilder {
+	        self.y = coordinate;
+	        self
+	    }
+
+	    fn radius(&mut self, radius: f64) -> &mut CircleBuilder {
+	        self.radius = radius;
+	        self
+	    }
+
+	    fn finalize(&self) -> Circle {
+	        Circle { x: self.x, y: self.y, radius: self.radius }
+	    }
+	}
+
+	c = CircleBuilder::new().x(1.0)
+                .y(2.0)
+                .radius(2.0)
+                .finalize();;
+    println!("{}", c.reference().area());
+	
+
 }
 
 //function
