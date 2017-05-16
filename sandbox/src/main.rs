23c1066ba1bcaea86cb01df36f6951fd13fc2284
Guillaume Guimond <guimog@gmail.com>
2017-05-16 15:40:42 +0200

    guarantees

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -901,4 +901,16 @@ fn main() {
     for _ in 0..10 {
         rx2.recv().unwrap();
     }
+
+    use std::cell::Cell;
+
+	let x = Cell::new(1);
+	let y = &x;
+	let z = &x;
+	x.set(2);
+	y.set(3);
+	z.set(4);
+	println!("{}", x.get());
+
+	
 }
