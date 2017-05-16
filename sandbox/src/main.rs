f3f2054a9bd6627f61906dfa8bccb40710cca02c
Guillaume Guimond <guimog@gmail.com>
2017-05-16 11:45:14 +0200

    iterators

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -809,4 +809,52 @@ fn main() {
 	if cfg!(target_os = "macos") || cfg!(target_os = "ios") {
 	    println!("Think Different!");
 	}
+
+	let mut range = 0..10;
+
+	loop {
+	    match range.next() {
+	        Some(x) => {
+	            println!("{}", x);
+	        },
+	        None => { break }
+	    }
+	}
+
+	let nums = vec![1, 2, 3];
+	for num in &nums {
+	    println!("{}", num);
+	}
+
+	let one_to_one_hundred = (1..101).collect::<Vec<i32>>();
+	let one_to_one_hundred = (1..101).collect::<Vec<_>>();
+	let greater_than_forty_two = (0..100)
+                             .find(|x| *x > 42);
+
+    match greater_than_forty_two {
+	    Some(_) => println!("Found a match!"),
+	    None => println!("No match found :("),
+	}
+
+	let sum = (1..4).fold(0, |sum, x| sum + x);
+
+	for num in nums.iter() {
+	   println!("{}", num);
+	}
+
+	(1..100).map(|x| x + 1);
+
+	for i in (1..).take(5) {
+	    println!("{}", i);
+	}
+
+	for i in (1..100).filter(|&x| x % 2 == 0) {
+	    println!("{}", i);
+	}
+
+	(1..)
+    .filter(|&x| x % 2 == 0)
+    .filter(|&x| x % 3 == 0)
+    .take(5)
+    .collect::<Vec<i32>>();
 }
