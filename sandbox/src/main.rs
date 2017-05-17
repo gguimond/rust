f3c1e9ece7a6900b7991ac2e9c3db961a48e73b0
Guillaume Guimond <guimog@gmail.com>
2017-05-17 15:22:23 +0200

    procedural macros

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -965,4 +965,22 @@ fn main() {
 	fn foocxxc<T: AsRef<str>>(s: T) {
 	    let slice = s.as_ref();
 	}
+
+	//#[macro_use]
+	//extern crate hello_world_derive;
+
+	/*trait HelloWorld {
+	    fn hello_world();
+	}
+
+	#[derive(HelloWorld)]
+	struct FrenchToast;
+
+	#[derive(HelloWorld)]
+	struct Waffles;
+
+	fn main() {
+	    FrenchToast::hello_world();
+	    Waffles::hello_world();
+	}*/
 }
