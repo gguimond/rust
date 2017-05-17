820c328665493b37cef4bf0a11064707b0dcc556
Guillaume Guimond <guimog@gmail.com>
2017-05-17 10:54:51 +0200

    ffi

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -6,6 +6,8 @@
 extern crate phrases;
 pub use phrases::english::greetings::hello as hi;
 
+extern crate libc;
+
 use std::thread;
 
 mod test;
@@ -913,4 +915,29 @@ fn main() {
 	println!("{}", x.get());
 
 	
+	use libc::{c_int, size_t};
+
+	//#[link(name = "snappy")]
+	/*extern {
+	    fn snappy_compress(input: *const u8,
+	                       input_length: size_t,
+	                       compressed: *mut u8,
+	                       compressed_length: *mut size_t) -> c_int;
+	    fn snappy_uncompress(compressed: *const u8,
+	                         compressed_length: size_t,
+	                         uncompressed: *mut u8,
+	                         uncompressed_length: *mut size_t) -> c_int;
+	    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+	    fn snappy_uncompressed_length(compressed: *const u8,
+	                                  compressed_length: size_t,
+	                                  result: *mut size_t) -> c_int;
+	    fn snappy_validate_compressed_buffer(compressed: *const u8,
+	                                         compressed_length: size_t) -> c_int;
+	}
+
+	pub fn validate_compressed_buffer(src: &[u8]) -> bool {
+	    unsafe {
+	        snappy_validate_compressed_buffer(src.as_ptr(), src.len() as size_t) == 0
+	    }
+	}*/
 }
